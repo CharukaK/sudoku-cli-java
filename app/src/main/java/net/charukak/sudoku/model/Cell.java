d21d145dfd3a859d5cd89e2544cc4468d69cdec4
CharukaK <charukakarunanayake@gmail.com>
2026-05-30 13:42:31 +0530

    Update Cell implementation

--- app/src/main/java/net/charukak/sudoku/model/Cell.java
+++ app/src/main/java/net/charukak/sudoku/model/Cell.java
@@ -1,10 +1,13 @@
 package net.charukak.sudoku.model;
 
+import net.charukak.sudoku.utils.SudokuConstants;
+
 public class Cell implements PrintableElement {
     private int value;
-    private boolean isFixed;
+    private boolean isPreFilled;
 
-    public Cell(int value) {
+    public Cell(int value) throws SudokuError {
+        validateValue(value);
         this.value = value;
     }
 
@@ -17,18 +20,28 @@ public class Cell implements PrintableElement {
         return this.value;
     }
 
-    public void setIsFixed(boolean isFixed) {
-        this.isFixed = isFixed;
+    public void setPreFilled(boolean isPreFilled) {
+        this.isPreFilled = isPreFilled;
     }
 
-    public void setValue(int name) {
-        this.value = name;
-    }
-
-    public Object getIsFixed() throws SudokuError {
-        if (isFixed) {
-            throw new SudokuError("you cannot edit a fixed value string");
+    public void setValue(int value) throws SudokuError {
+        if (isPreFilled) {
+            throw new SudokuError("Cannot modify a pre-filled cell");
         }
-        return this.isFixed;
+
+        validateValue(value);
+        this.value = value;
+    }
+
+    public boolean isPreFilled() {
+        return this.isPreFilled;
+    }
+
+    private void validateValue(int value) throws SudokuError {
+        if (value < 0 || value > SudokuConstants.BOARD_LENGTH) {
+            throw new SudokuError(
+                    "Invalid number. Only accepts numbers from 0-"
+                            + SudokuConstants.BOARD_LENGTH);
+        }
     }
 }
--- app/src/test/java/net/charukak/sudoku/model/CellTest.java
+++ app/src/test/java/net/charukak/sudoku/model/CellTest.java
@@ -1,12 +1,13 @@
 package net.charukak.sudoku.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class CellTest {
     @Test
-    public void testCellValue() {
+    public void testCellValue() throws SudokuError {
         int value = 3;
         Cell cell = new Cell(value);
 
@@ -14,7 +15,7 @@ public class CellTest {
     }
 
     @Test
-    public void testPrintableVlaue() {
+    public void testPrintableValue() throws SudokuError {
         int value = 3;
         Cell cell = new Cell(value);
 
@@ -22,16 +23,48 @@ public class CellTest {
     }
 
     @Test
-    public void testPrintableVlaueForZero() {
+    public void testPrintableValueForZero() throws SudokuError {
         Cell cell = new Cell(0);
         assertEquals(cell.getPrintableString(), ".");
     }
 
     @Test(expected = SudokuError.class)
-    public void testFixedValueEditing() throws SudokuError {
+    public void testPreFilledValueEditing() throws SudokuError {
         Cell cell = new Cell(1);
-        cell.setIsFixed(true);
-        assertEquals(cell.getIsFixed(), true);
+        cell.setPreFilled(true);
+        assertTrue(cell.isPreFilled());
         cell.setValue(5);
+    }
+
+    @Test(expected = SudokuError.class)
+    public void testSettingInvalidValue() throws SudokuError {
+        Cell cell = new Cell(1);
+        cell.setValue(12);
+    }
+
+    @Test(expected = SudokuError.class)
+    public void testSettingInvalidNegativeValue() throws SudokuError {
+        Cell cell = new Cell(1);
+        cell.setValue(-1);
+    }
+
+    @Test
+    public void testSettingBoundaryValues() throws SudokuError {
+        Cell cell = new Cell(1);
+        cell.setValue(9);
+        assertEquals(cell.getValue(), 9);
+
+        cell.setValue(0);
+        assertEquals(cell.getValue(), 0);
+    }
+
+    @Test(expected = SudokuError.class)
+    public void testConstructorRejectsInvalidHighValue() throws SudokuError {
+        new Cell(12);
+    }
+
+    @Test(expected = SudokuError.class)
+    public void testConstructorRejectsInvalidNegativeValue() throws SudokuError {
+        new Cell(-1);
     }
 }
