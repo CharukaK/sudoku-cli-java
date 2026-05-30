97534bda1263a02769f91e1e519e023519c3367e
CharukaK <charukakarunanayake@gmail.com>
2026-05-30 16:20:44 +0530

    Add implementations for setting values for board

--- app/src/main/java/net/charukak/sudoku/model/Board.java
+++ app/src/main/java/net/charukak/sudoku/model/Board.java
@@ -16,19 +16,33 @@ public class Board implements PrintableElement {
         throw new UnsupportedOperationException("Unimplemented method 'getPrintableString'");
     }
 
-    public Integer getValue(int row, int col) {
-        return board[row][col].getValue();
+    public int getValue(int row, int col) throws SudokuError {
+        validatePosition(row, col);
+        return this.board[row - 1][col - 1].getValue();
+    }
+
+    public void setValue(int row, int col, int value) throws SudokuError {
+        validatePosition(row, col);
+        this.board[row - 1][col - 1].setValue(value);
     }
 
     private void initializeBoard() {
         for (int row = 0; row < SudokuConstants.BOARD_LENGTH; row++) {
             for (int col = 0; col < SudokuConstants.BOARD_LENGTH; col++) {
                 try {
-                    board[row][col] = new Cell(0);
+                    this.board[row][col] = new Cell(0);
                 } catch (Exception e) {
                     throw new IllegalStateException("failed initialize empty cell");
                 }
             }
         }
     }
+
+    private void validatePosition(int row, int col) throws SudokuError {
+        if (row < 1 || row > SudokuConstants.BOARD_LENGTH
+                || col < 1 || col > SudokuConstants.BOARD_LENGTH) {
+            throw new SudokuError("Invalid board position: (" + row + "," + col + ")");
+        }
+    }
+
 }
--- app/src/test/java/net/charukak/sudoku/model/BoardTest.java
+++ app/src/test/java/net/charukak/sudoku/model/BoardTest.java
@@ -1,9 +1,15 @@
 package net.charukak.sudoku.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import net.charukak.sudoku.utils.SudokuConstants;
 
@@ -12,13 +18,61 @@ public class BoardTest {
 
     @Test
     @DisplayName("Initializes a 9x9 board with all values set to Zero")
-    void createAValidBoard() {
+    void testCreatingAValidBoard() throws SudokuError {
         Board board = new Board();
 
-        for (int row = 0; row < SudokuConstants.BOARD_LENGTH; row++) {
-            for (int col = 0; col < SudokuConstants.BOARD_LENGTH; col++) {
+        // implementation is 1 index
+        for (int row = 1; row <= SudokuConstants.BOARD_LENGTH; row++) {
+            for (int col = 1; col <= SudokuConstants.BOARD_LENGTH; col++) {
                 assertEquals(0, board.getValue(row, col));
             }
         }
     }
+
+    @Test
+    @DisplayName("Set/Get value methods are working properly")
+    void testValueSetterAndGetter() throws SudokuError {
+        Board board = new Board();
+        board.setValue(3, 3, 7);
+        assertEquals(7, board.getValue(3, 3));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Test out of bounds value getter")
+    @MethodSource("getOutOfBoundTestCases")
+    void testOutOfBoundValueGetter(int row, int col) {
+        Board board = new Board();
+        assertThrows(SudokuError.class, () -> board.getValue(row, col));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Test out of bounds value setter")
+    @MethodSource("getOutOfBoundTestCases")
+    void testOutOfBoundValueSetter(int row, int col) {
+        Board board = new Board();
+        assertThrows(SudokuError.class, () -> board.setValue(row, col, 5));
+    }
+
+    static Stream<Arguments> getOutOfBoundTestCases() {
+        return Stream.of(
+                Arguments.of(0, 12),
+                Arguments.of(-12, 0),
+                Arguments.of(0, 1),
+                Arguments.of(1, 0),
+                Arguments.of(10, 1),
+                Arguments.of(1, 10),
+                Arguments.of(-1, 1),
+                Arguments.of(1, -1));
+    }
+
+    @Test
+    @DisplayName("Set/Get at boundary positions")
+    void testBoundaryPositions() throws SudokuError {
+        Board board = new Board();
+        board.setValue(1, 1, 5);
+        assertEquals(5, board.getValue(1, 1));
+        board.setValue(9, 9, 3);
+        assertEquals(3, board.getValue(9, 9));
+    }
+
 }
